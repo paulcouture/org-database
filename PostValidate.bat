b012377777f1f5b7e8eac2405e3b0d8bd6fa0830
paulcouture <paul.couture@mail.com>
2020-01-14 13:54:08 +0330

    7ac148148cad616c144ca81121d33a4ef4f628c1

--- PostValidate.bat
+++
@@ -1,3 +0,0 @@
-cd c:\
-copy "C:\ProgramData\git\org-database\Update.xml" "C:\programdata"\
-start "" /D "C:\programdata" "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "C:\programdata\Update.xml" 
